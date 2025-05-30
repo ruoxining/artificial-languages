3a683a280e8f84133127c6c491493cb32efb08ca
ruoxining <ruoxining@outlook.com>
2025-05-30 09:30:51 -0400

    [fix] fix paths

--- data_gen/gen_data.sh
+++ data_gen/gen_data.sh
@@ -1,5 +1,9 @@
 python sample_sentences.py -g base-grammar.gr -n 100000 -O . -b True
 python permute_sentences.py -s sample_base-grammar.txt -O permuted_samples/ 
+mkdir -p permuted_splits/base
+mkdir -p permuted_splits/1-1
+mkdir -p permuted_splits/1-2
+mkdir -p permuted_splits/1-3
 python make_splits.py -S permuted_samples -O permuted_splits/base -n 5
 python make_splits.py -S permuted_samples -O permuted_splits/1-1 -n 10
 python make_splits.py -S permuted_samples -O permuted_splits/1-2 -n 2
