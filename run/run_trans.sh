cfc06807ab2f8a5128996d280216ceb6105db105
ruoxining <ruoxining@outlook.com>
2025-05-30 15:37:15 -0400

    [chore] add base run

--- run/run_trans.sh
+++ run/run_trans.sh
@@ -1,3 +1,5 @@
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name transformer-base
+
 python3 run/run_all_jobs.py -s 3 -n 6 --job_name transformer-1-1
 python3 run/run_all_jobs.py -s 2 -n 6 --job_name transformer-1-2
 python3 run/run_all_jobs.py -s 1 -n 6 --job_name transformer-1-3
