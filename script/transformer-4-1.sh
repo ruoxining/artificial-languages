4a0e6c14f5f1aa30b03c025bc318d587c05a59d7
RogerZhu <y485zhu@uwaterloo.ca>
2025-06-03 02:50:52 -0400

    update changes

--- script/transformer-4-1.sh
+++ script/transformer-4-1.sh
@@ -56,7 +56,7 @@ TRAIN_CMD="fairseq-train --task language_modeling \"data-bin/base/${GRAMMAR}/${S
     --power 1.0 \
     --warmup-updates 400 \
     --clip-norm 0.0 \
-    --end_learning_rate 1e-7 \
+    --end-learning-rate 1e-7 \
     --tokens-per-sample 128 \
     --sample-break-mode none \
     --max-tokens 512 \
