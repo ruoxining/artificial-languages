5420bc665106367731173d976d05750a30d835f3
ruoxining <ruoxining@outlook.com>
2025-06-04 03:10:55 -0400

    [feat] add extended experiments

---
+++ run/run_trans_extended.sh
@@ -0,0 +1,16 @@
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-3-3
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-3-4
+
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-5-4
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-5-5
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-5-6
+
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-6-4
+
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-7-4
+
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-12-3
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-12-4
+
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-13-2
+python3 run/run_all_jobs.py -s 3 -n 6 --job_name lstm-13-3
